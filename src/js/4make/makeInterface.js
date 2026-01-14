9182d062baf09a42d22d9e930150b7831dbe3e67
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-14 05:35:59 -0500

    update

--- src/js/4make/makeInterface.js
+++ src/js/4make/makeInterface.js
@@ -2,6 +2,19 @@
 
 function makeInterface(whichArray, whichYear)
 {
+    let titleAndLink = ce('a');
+    titleAndLink.href = 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_Calendar';
+    titleAndLink.target = '_blank';
+    titleAndLink.style.fontFamily = 'Arial';
+    titleAndLink.style.fontWeight = 'bold';
+    titleAndLink.textContent = 'CATopalian JavaScript Calendar';
+    titleAndLink.style.fontSize = '14px';
+    titleAndLink.style.textDecoration = 'none';
+    titleAndLink.style.color = 'rgb(0, 255, 255)';
+    ba(titleAndLink);
+
+    //-//
+
     let mainDiv = ce('div');
     mainDiv.id = 'mainDiv';
     mainDiv.style.display = 'flex';
@@ -66,7 +79,7 @@ function makeInterface(whichArray, whichYear)
 //----//
 
 // Dedicated to God the Father
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 // https://github.com/ChristopherTopalian
 // https://github.com/ChristopherAndrewTopalian
 // https://sites.google.com/view/CollegeOfScripting
